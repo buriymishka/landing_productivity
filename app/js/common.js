0e4a89ff4b84512a6fb5dc414a510f543b5a90da
buriymishka <55351111+buriymishka@users.noreply.github.com>
2020-09-28 14:20:42 +0300

    add header with adaptive

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-	
+	let setupMobileMenu = () => {
+    let menuBtn = document.querySelector('.menu-btn');
+    let mobileMenu =document.querySelector('.menu-mobile');
+    let mobileMenuList = document.querySelector('.menu-mobile__navigation');
+    let menuList = document.querySelector('.header__navigation-list');
+    mobileMenuList.append(menuList.cloneNode(true));
+
+    let socials = document.querySelector('.header__social');
+    mobileMenuList.append(socials.cloneNode(true));
+    // mobileMenuList.querySelector('.header__nav-list').classList.remove('main-menu');
+
+    let links = mobileMenuList.querySelectorAll('.header__navigation-item a');
+    links.forEach((el) => {
+      el.addEventListener('click', (e) => {
+        closeMenu(e)
+      })
+    })
+
+    menuBtn.addEventListener('click', function(e){
+      closeMenu(e);
+    });
+
+    let closeMenu = (e) => {
+      e.preventDefault();
+      menuBtn.classList.toggle('active');
+      menuBtn.classList.toggle('on');
+      mobileMenu.classList.toggle('active');
+    };
+  }
+  setupMobileMenu();
+
+  let headerTitle = document.querySelector('.header__title');
+  headerTitle.innerHTML = headerTitle.innerHTML.replace(/productivity/, '<span class="strong">productivity</span>');
+
 });

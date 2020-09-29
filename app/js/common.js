fd5ccfa812ef2c25b68c443673a23c0278e52c72
buriymishka <55351111+buriymishka@users.noreply.github.com>
2020-09-29 13:11:22 +0300

    added sixth and seveth sections

--- app/js/common.js
+++ app/js/common.js
@@ -33,16 +33,67 @@ document.addEventListener("DOMContentLoaded", () => {
   let headerTitle = document.querySelector('.header__title');
   headerTitle.innerHTML = headerTitle.innerHTML.replace(/productivity/, '<span class="strong">productivity</span>');
 
-  const swiper = new Swiper('.swiper-container', {
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: true,
-    },
-  });
+  // const swiper = new Swiper('.swiper-container', {
+  //   loop: true,
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     clickable: true,
+  //   },
+  //   autoplay: {
+  //     delay: 3000,
+  //     disableOnInteraction: true,
+  //   },
+  // });
+
+  // const swiper = new Swiper('.swiper-container1', {
+  //   loop: true,
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     clickable: true,
+  //   },
+  //   autoplay: {
+  //     delay: 3000,
+  //     disableOnInteraction: true,
+  //   },
+  // });
+
+
+  let setAccordion = () => {
+    
+    let questionList = document.querySelectorAll('.questions__item');
+    let faqClick = (question, answer) => {
+      if(question.classList.contains('active')){
+        question.classList.remove('active');
+        answer.classList.remove('active');
+      }else{
+        closeOpenTabs();
+        question.classList.add('active');
+        answer.classList.add('active');
+      }
+      if (answer.clientHeight) {
+        answer.style.height = 0;
+      } else {
+        let wrapper = document.querySelector('.questions__answer-wrapper');
+        answer.style.height = wrapper.clientHeight + "px";
+      }
+    }
+
+    let closeOpenTabs = () => {
+      let ask = document.querySelector('.questions__ask.active');
+      let answer = document.querySelector('.questions__answer.active');
+      if(ask && answer){
+        ask.classList.remove('active');
+        answer.classList.remove('active');
+        answer.style.height = 0;
+      }
+    }
+    questionList.forEach(el => {
+      let question = el.querySelector('.questions__ask');
+      let answer = el.querySelector('.questions__answer');
+
+      question.addEventListener('click', () => faqClick(question, answer))
+    });
+  };
+  setAccordion();
 
 });
